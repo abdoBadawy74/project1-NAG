d76fd95253a36943ce67a397c9456b6dde7008e1
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-05-02 19:31:15 +0300

    create new files, error handling

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Services</li>
+      </ul>
+    </header>
+  );
+}
+export default Header;
