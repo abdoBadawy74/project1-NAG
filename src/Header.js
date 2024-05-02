96cf793a395d318d24ae48693de3d85489834eae
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-05-02 20:22:08 +0300

    dealing with imgs

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,5 @@
+import myImage from "./img.jpg"
+
 function Header() {
   return (
     <header>
@@ -7,6 +9,12 @@ function Header() {
         <li>Contact</li>
         <li>Services</li>
       </ul>
+      {/* <img src="./img.png" alt="logo" />   ==>> this is incorrect */}
+
+
+        {/* <img src={myImage} alt="logo" />  first way to add image in react */}
+
+        <img src={require('./img.jpg')} alt="logo" /> {/* second way to add image in react */}
     </header>
   );
 }
