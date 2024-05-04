fdf9c2c83d5e99e3b2399a87be4669c686267ce1
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-05-04 05:53:17 +0300

    nav p2 , dealing with fonts

---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Content() {
+  return (
+    <div>
+      <p>Some Info About React</p>
+      <ul>
+        <li>React is a JavaScript library for building user interfaces</li>
+        <li>React is used to build single page applications</li>
+        <li>React allows us to create reusable UI components</li>
+        <li>React is maintained by Facebook</li>
+      </ul>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Style.css";
 import NavBar from "./Components/NavBar";
+import Content from "./Content";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>
+root.render(<div className="contianer">
     <NavBar />
+    <Content />
 </div>);
